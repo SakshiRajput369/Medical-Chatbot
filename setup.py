7a4880412bf9563e0114a7ec708a4e264df895e6
Sakshi Singh <your_github_email@example.com>
2025-10-06 02:40:34 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Sakshi Singh',
+    author_email= 'sakshis.ec.22@nitj.ac.in',
+    packages= find_packages(),
+    install_requires = []
+)
